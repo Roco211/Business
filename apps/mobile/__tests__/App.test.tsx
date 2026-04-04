67ef649b336a0a24df505d62590ad7a34ac18076
Roco Luo <luojianhuixo@gmai.com>
2026-04-05 01:13:58 +0800

    test: isolate app shell screen mocks

--- apps/mobile/__tests__/App.test.tsx
+++ apps/mobile/__tests__/App.test.tsx
@@ -2,6 +2,31 @@ import { render, screen } from "@testing-library/react-native";
 
 import App from "../App";
 
+
+jest.mock("../src/features/dashboard/screens/DashboardScreen", () => {
+  const ReactNative = require("react-native");
+  return function MockDashboardScreen() {
+    return <ReactNative.Text>Mock Dashboard Screen</ReactNative.Text>;
+  };
+});
+
+
+jest.mock("../src/features/chat/screens/ChatScreen", () => {
+  const ReactNative = require("react-native");
+  return function MockChatScreen() {
+    return <ReactNative.Text>Mock Chat Screen</ReactNative.Text>;
+  };
+});
+
+
+jest.mock("../src/features/ledger/screens/LedgerScreen", () => {
+  const ReactNative = require("react-native");
+  return function MockLedgerScreen() {
+    return <ReactNative.Text>Mock Ledger Screen</ReactNative.Text>;
+  };
+});
+
+
 test("renders the three primary screen labels", () => {
   render(<App />);
 
